1bc49811c10dc68f51372b4f7af308ac1895879c
Taabish0908 <taabishanwar0908@gmail.com>
2022-04-05 16:06:46 +0530

    fixed some errors

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -2,14 +2,41 @@ const urlModel = require("../models/urlModel")
 const shortid = require("shortid");
 const validUrl = require("valid-url");
 
+const isValid = function(value){
+    if(typeof value ==='undefined' || typeof value === null) return false
+    if(typeof value === 'string' && value.trim().length ===0) return false
+    return true
+}
 
 
+const isValidLink = function(value) {
+    if (!(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%.\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%\+.~#?&//=]*)/g.test(value.trim()))) {
+        return false
+    }
+    return true
+}
 
 const createUrl = async function(req, res){
 
 
     const longUrl = req.body.longUrl;
     const baseUrl = 'http://localhost:3000'
+    const shortUrl = req.body.shortUrl
+    const urlCode = req.body.urlCode
+
+   
+
+
+    if(Object.keys(req.body).length==0){
+        return res.status(400).send("pleae enter data in the request body ")
+        
+    }
+
+    if(!isValidLink(longUrl)){
+        return res.status(400).send("Invalid URL. Please enter a valid url for shortening.. ")
+    }
+    
+
 
     
     console.log("base url " + baseUrl + "   " + longUrl);
@@ -17,11 +44,24 @@ const createUrl = async function(req, res){
         return res.status(401).send("Internal error. Please come back later.");
     }
 
-    const urlCode = shortid.generate();
+    if(isValid(urlCode)||(shortUrl)){
+        return res.status(400).send("you are not allowed to enter shorturl or urlcode ")
+        
+    }
+    // if(isValid(shortUrl)){
+    //     return res.status(400).send("you are not allowed to enter shortUrl here ") 
+    // }
+    
+    // if(isValid(urlCode)){
+    //     return res.status(400).send("you are not allowed to enter Urlcode here ")
+    // }
+    
 
+    
     if(validUrl.isUri(longUrl)){
 
         try{
+            const urlCode = shortid.generate();
             const myurl = await urlModel.findOne({longUrl : longUrl}).select({longUrl:1,shortUrl:1,urlCode:1,_id:0})
             console.log(myurl);
             if(myurl){
@@ -29,7 +69,9 @@ const createUrl = async function(req, res){
             }else{
 
                 const shortUrl = baseUrl + "/" + urlCode;
-                newurl  = new urlModel({
+
+                
+               const  newurl  = ({
                     longUrl,
                     shortUrl,
                     urlCode,
@@ -37,14 +79,15 @@ const createUrl = async function(req, res){
                 });
                 
                 await urlModel.create(newurl)
-                return res.status(201).send(newurl);
+                const newdata = {longUrl:newurl.longUrl,shortUrl:newurl.shortUrl,urlCode:newurl.urlCode}
+                return res.status(201).send({status:true,data:newdata});
             }
         }catch(err){
             console.error(err.message);
             return res.status(500).send("Internal Server error " + err.message);
         }
     }else{
-        res.status(400).send("Invalid URL. Please enter a vlaid url for shortening.");
+        res.status(400).send("Invalid URL. Please enter a valid url for shortening.");
     }    
 };
 
